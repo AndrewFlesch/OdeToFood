861ff5618b68bd2d5a977104fa2e5110b7926ed0
Andrew Flesch <aflesch@outlook.com>
2018-03-04 22:19:16 -0600

    Views Module

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Hello from the HOme Controller";
+            var model = new Restaurant { Id = 1, Name = "Andrews Pizza Place" };
+            return View(model);
         }
     }
 }
