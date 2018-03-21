1df2ad8b9e1aaff34562e8a7967984e32912bd71
Andrew Flesch <aflesch@outlook.com>
2018-03-20 21:07:07 -0500

    Finished Module 5 Models

--- OdeToFood/Models/ResturantEditModel.cs
+++ OdeToFood/Models/ResturantEditModel.cs
@@ -1,20 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace OdeToFood.Models
 {
-    public class Restaurant
+    public class ResturantEditModel
     {
-        public int Id { get; set; }
-
-        [Display(Name="Restaurant Name")]
         [Required, MaxLength(80)]
         public string Name { get; set; }
         public CusineType Cuisine { get; set; }
-
-
     }
 }
